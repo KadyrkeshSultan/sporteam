5263a90ea4b601ad26e2c3351e83cbb0222d8a5c
Sultan Kadyrkesh <sultankadyr@outlook.com>
2019-01-23 12:56:36 +0600

    fix style material

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
-import Dashboard from './components/dashboard/Dashboard'
-import ProjectDetails from './components/projects/ProjectDetails'
 import SignIn from './components/auth/SignIn'
-import SignUp from './components/auth/SignUp'
-import CreateProject from './components/projects/CreateProject'
 import ButtonAppBar from './components/layout/ButtonAppBar'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div >
+        <div className="App">
           <ButtonAppBar />
           <Switch>
             <Route path='/login' component={SignIn} />
