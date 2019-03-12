e88abff7e43596e2d236f69e8f7182b0efd0c1c2
interglare <interglare@gmail.com>
2019-03-12 18:03:00 +0600

    размер картинки,карточки под react-materialize, add firebase

--- src/components/mainboard/EventCard.js
+++ src/components/mainboard/EventCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Card, CardTitle, Col } from 'react-materialize'
 
-class EventCard extends React.Component{
-    render(){
+class EventCard extends React.Component {
+    render() {
         const { date, name, sportType, location, image } = this.props;
 
         return <React.Fragment>
-            <div class="col s12 m6 l3">
+            {/*<div class="col s12 m6 l3">
                 <div class="card small">
                     <div class="card-image">
                         <img src={image} />
@@ -17,7 +18,15 @@ class EventCard extends React.Component{
                         <p><i class="fas fa-location-arrow"></i>{location}</p>
                     </div>
                 </div>
-            </div>
+    </div>*/}
+            <Col s={12} m={6} l={3}>
+                <Card className='small'
+                    header={<CardTitle image={image}>{date}</CardTitle>}>
+                    <p><b>{name}</b></p>
+                    <p>{sportType}</p>
+                    <p><i class="fas fa-location-arrow"></i>{location}</p>
+                </Card>
+            </Col>
         </React.Fragment>
     }
 }
