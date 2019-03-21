709e02f09d0158c4827c16b583e0ac4ee58fe11f
interglare <interglare@gmail.com>
2019-03-21 17:27:16 +0600

    Добавил карусель получше с текстом

--- src/components/mainboard/CarouselMain.js
+++ src/components/mainboard/CarouselMain.js
@@ -1,85 +1,38 @@
-import React, { Component } from "react";
-import Slider from "react-slick";
-import { Card, CardMedia } from "@material-ui/core";
-
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 export default class CarouselMain extends Component {
   render() {
-    const settings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
-    const styles = {
-      card: {
-        position: 'relative',
-      },
-      overlay: {
-        position: 'absolute',
-        top: '100px',
-        left: '20px',
-        color: 'white',
-        textShadow: "1px 1px 1px black",
-        fontSize: 18,
-        bottom: 0,
-        minHeight: 80,
-        width: '100%',
-        zIndex: '2',
-
-      }
-    }
     return (
 
       <div ><br />
         <h2 style={{ fontWeight: '400' }}>ПОСЛЕДНИЕ НОВОСТИ</h2>
-        <Slider {...settings} >
+        <Carousel showArrows={false} autoPlay={true} showThumbs={false} dynamicHeight={false} useKeyboardArrows={true} swipeable={true} emulateTouch={true} showStatus={false}>
           <div >
-            {/* <Card style={{ height: '300px', position: 'relative', boxShadow:'0px 0px 0px',backgroundColor:'' }}>
-              <CardMedia component="img"
-                alt="Contemplative Reptile"
-                image={"http://cdn.chatsports.com/thumbnails/5091-69818-original.jpeg"} />
-              <div style={styles.overlay}>
-                <h2 >Firebase</h2>
-              </div>
-            </Card> */}
-            <img src="http://cdn.chatsports.com/thumbnails/5091-69818-original.jpeg" style={{width:'100%',maxHeight:'400px'}}/>
-            <div style={{
-                bottom: 0,
-                minHeight: 80,
-                left: 0,
-                position: 'absolute',
-                width: '100%',
-                zIndex: '2'
-              }}></div>
+            <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
+            <p className="legend" style={{backgroundColor:'#0000'}}>Legend 1</p>
           </div>
-          <div >
-            {/* <Card style={{ height: '300px' }}>
-              <CardMedia component="img"
-                alt="Contemplative Reptile"
-                image={"http://cdn.chatsports.com/thumbnails/5091-69818-original.jpeg"} />
-              <div style={{
-                bottom: 0,
-                minHeight: 80,
-                left: 0,
-                position: 'absolute',
-                width: '100%',
-                zIndex: '2'
-              }}>
-                <h2>Firebase</h2>
-              </div>
-            </Card> */}
-            <img src="http://cdn.chatsports.com/thumbnails/5091-69818-original.jpeg" style={{width:'100%',maxHeight:'400px'}}/>
-            <div style={{
-                bottom: 0,
-                minHeight: 80,
-                left: 0,
-                position: 'absolute',
-                width: '100%',
-                zIndex: '2'
-              }}></div>
+          <div>
+          <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
+            <p className="legend" style={{backgroundColor:'#0000'}}>Legend 2</p>
           </div>
-        </Slider>
+          <div>
+          <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
+            <p className="legend" style={{backgroundColor:'#0000'}}>Legend 3</p>
+          </div>
+          <div>
+          <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
+            <p className="legend" style={{backgroundColor:'#0000'}}>Legend 4</p>
+          </div>
+          <div>
+          <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
+            <p className="legend" style={{backgroundColor:'#0000'}}>Legend 5</p>
+          </div>
+          <div>
+          <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
+            <p className="legend" style={{backgroundColor:'#0000'}}>Legend 6</p>
+          </div>
+        </Carousel>
       </div >
     );
   }
