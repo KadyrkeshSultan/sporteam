b217664ff332e0de4a751ccfdf30c0f419e00ebc
Sultan Kadyrkesh <sultankadyr@outlook.com>
2019-03-31 01:11:51 +0600

    Карты в Areaboard

--- src/components/area/AreaBoard.js
+++ src/components/area/AreaBoard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
-import { Grid, Typography, Link} from '@material-ui/core';
+import { Grid, Typography, Link } from '@material-ui/core';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
@@ -9,6 +9,7 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import YandexShare from '../layout/YandexShare';
+import { YMaps, Map, GeoObject } from 'react-yandex-maps';
 
 const styles = theme => ({
     icon: {
@@ -24,88 +25,104 @@ const styles = theme => ({
             marginRight: 'auto',
         },
     },
-    
-    
+
+
 });
 
 class AreaBoard extends React.Component {
     render() {
 
-        const { classes,theme, area, areaTypes, sportTypes } = this.props;
+        const { classes, theme, area, areaTypes, sportTypes } = this.props;
         const areaType = area.id && (areaTypes && areaTypes.find(item => item.id == area.type.id));
         const sports = area.id && (sportTypes && sportTypes.filter(item => area.sportTypes.find(i => i.id == item.id) != undefined));
-        console.log('areaType', areaTypes);
-        console.log('sports', sports);
+        const latitude = area.id && area.location.latitude;
+        const longitude = area.id && area.location.longitude;
+        const center = (latitude != null && latitude != '') ? [latitude, longitude] : [51.132435, 71.404126];
+        const location = {
+            center: center,
+            zoom: 16
+        }
         return <React.Fragment>
             <div className={classNames(classes.layout, classes.cardGrid)}>
-            <Grid container justify='space-between'>
-                <Grid item xs={12} sm={6}>
-                <h2 style={{ fontWeight: '400' }}>{area.id && area.name}</h2>
-                <h3><YandexShare /></h3>
+                <Grid container justify='space-between'>
+                    <Grid item xs={12} sm={6}>
+                        <h2 style={{ fontWeight: '400' }}>{area.id && area.name}</h2>
+                        <h3><YandexShare /></h3>
+                    </Grid>
                 </Grid>
-            </Grid>
-            <Carousel showArrows={true} autoPlay={true} showThumbs={false} dynamicHeight={false} useKeyboardArrows={true} swipeable={true} emulateTouch={true} showStatus={false}>
-            {
-                area.id && area.images.map(item => {
-                    return <div key={item.filename}>
-                        <img src={item.url} style={{height:'300px',objectFit:'cover'}} />
-                    </div>
-                })
-            }
-        </Carousel>
-            <br/>
-            <Grid container spacing={24}>
-                <Grid item xs={12} md={6} style={{paddingLeft:'30px'}}>
-                    <Typography component="p"><i className="fas fa-location-arrow"></i> {area.id && area.location.address}</Typography>                    
+                <Carousel showArrows={true} autoPlay={true} showThumbs={false} dynamicHeight={false} useKeyboardArrows={true} swipeable={true} emulateTouch={true} showStatus={false}>
+                    {
+                        area.id && area.images.map(item => {
+                            return <div key={item.filename}>
+                                <img src={item.url} style={{ height: '300px', objectFit: 'cover' }} />
+                            </div>
+                        })
+                    }
+                </Carousel>
+                <br />
+                <Grid container spacing={24}>
+                    <Grid item xs={12} md={6} style={{ paddingLeft: '30px' }}>
+                        <Typography component="p"><i className="fas fa-location-arrow"></i> {area.id && area.location.address}</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <blockquote>
+                            <Typography component="p" align="justify">
+                                {area.id && area.description}
+                            </Typography>
+                        </blockquote>
+                    </Grid>
+                    <Grid item xs={12} md={3}>
+                        <Typography component="h4"><b>Режим работы</b></Typography><br />
+                        <Typography component="p" >{area.id && area.worktime}</Typography>
+                    </Grid>
+                    <Grid item xs={12} md={3}>
+                        <Typography component="h4"><b>Характеристики</b></Typography><br />
+                        <Typography component="p" ><b> Тип площадки:</b> {areaType && areaType.name}</Typography>
+                    </Grid>
+                    <Grid item xs={12} md={3}>
+                        <Typography component="h4"><b>Стоимость</b></Typography><br />
+                        <Typography component="p" > {area.id && area.price}</Typography>
+
+                    </Grid>
+                    <Grid item xs={12} md={3}>
+                        <Typography component="h4"><b>Контакты</b></Typography><br />
+                        <Typography component="p" > {area.id && area.contacts}</Typography>
+
+                    </Grid>
+                    <Grid item xs={2} style={{ textAlign: 'center', letterSpacing: '40px' }}>
+                        <i className="fas fa-lightbulb" style={{ fontSize: '28px', color: 'lightgreen' }}></i>
+                    </Grid>
+                    <Grid item xs={2} style={{ textAlign: 'center', letterSpacing: '40px' }}>
+                        <i className="fas fa-wifi" style={{ fontSize: '28px', color: 'lightgreen' }}></i>
+                    </Grid>
+                    <Grid item xs={2} style={{ textAlign: 'center', letterSpacing: '40px' }}>
+                        <i className="fas fa-coffee" style={{ fontSize: '28px', color: 'lightgreen' }}></i>
+                    </Grid>
+                    <Grid item xs={2} style={{ textAlign: 'center', letterSpacing: '40px' }}>
+                        <i className="fas fa-car" style={{ fontSize: '28px', color: 'lightgreen' }}></i>
+                    </Grid>
+                    <Grid item xs={2} style={{ textAlign: 'center', letterSpacing: '40px' }}>
+                        <i className="fas fa-medkit" style={{ fontSize: '28px', color: 'lightgreen' }}></i>
+                    </Grid>
+                    <Grid item xs={2} style={{ textAlign: 'center', letterSpacing: '40px' }}>
+                        <i className="fas fa-shower" style={{ fontSize: '28px', color: 'lightgreen' }}></i>
+                    </Grid>
+
                 </Grid>
-                <Grid item xs={12}>
-                    <blockquote>
-                        <Typography component="p" align="justify">
-                        {area.id && area.description}
-                        </Typography>
-                    </blockquote>
-                </Grid>
-                <Grid item xs={12} md={3}>
-                <Typography component="h4"><b>Режим работы</b></Typography><br/>
-                <Typography component="p" >{area.id && area.worktime}</Typography>
-                </Grid>
-                <Grid item xs={12} md={3}>
-                <Typography component="h4"><b>Характеристики</b></Typography><br/>
-                <Typography component="p" ><b> Тип площадки:</b> {areaType && areaType.name}</Typography>  
-                </Grid>
-                <Grid item xs={12} md={3}>
-                <Typography component="h4"><b>Стоимость</b></Typography><br/>
-                <Typography component="p" > {area.id && area.price}</Typography>
-                
-                </Grid>
-                <Grid item xs={12} md={3}>
-                <Typography component="h4"><b>Контакты</b></Typography><br/>
-                <Typography component="p" > {area.id && area.contacts}</Typography>
-                
-                </Grid>
-                <Grid item xs={2} style={{textAlign:'center',letterSpacing:'40px'}}>
-                <i className="fas fa-lightbulb" style={{fontSize:'28px',color:'lightgreen'}}></i>
-                </Grid>
-                <Grid item xs={2} style={{textAlign:'center',letterSpacing:'40px'}}>
-                <i className="fas fa-wifi" style={{fontSize:'28px',color:'lightgreen'}}></i>
-                </Grid>
-                <Grid item xs={2} style={{textAlign:'center',letterSpacing:'40px'}}>
-                <i className="fas fa-coffee" style={{fontSize:'28px',color:'lightgreen'}}></i>
-                </Grid>
-                <Grid item xs={2} style={{textAlign:'center',letterSpacing:'40px'}}>
-                <i className="fas fa-car" style={{fontSize:'28px',color:'lightgreen'}}></i>
-                </Grid>
-                <Grid item xs={2} style={{textAlign:'center',letterSpacing:'40px'}}>
-                <i className="fas fa-medkit" style={{fontSize:'28px',color:'lightgreen'}}></i>
-                </Grid>
-                <Grid item xs={2} style={{textAlign:'center',letterSpacing:'40px'}}>
-                <i className="fas fa-shower" style={{fontSize:'28px',color:'lightgreen'}}></i>
-                </Grid>
-                
-            </Grid>
             </div>
-            <br/>
-            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Af5a217ff4e2b68222d3a6fc45d1d58611a8dfefa31b95d6ec074d1a3a2ec6d08&amp;source=constructor" width="100%" height="400" frameBorder="0"></iframe>
+            <br />
+            <Grid style={{ margin: '0 auto', textAlign: 'center', width: '90%' }}>
+                <YMaps>
+                    <Map width={'100%'} height={400} defaultState={location}>
+                        <GeoObject
+                            geometry={{
+                                type: 'Point',
+                                coordinates: location.center,
+                            }}
+                        />
+                    </Map>
+                </YMaps>
+            </Grid>
         </React.Fragment>
     }
 }
@@ -126,6 +143,6 @@ export default compose(
     connect(mapStateToProps),
     firestoreConnect([
         { collection: 'categorySports' },
-        { collection: 'sportgroundstype'}
+        { collection: 'sportgroundstype' }
     ])
 )(AreaBoard)
