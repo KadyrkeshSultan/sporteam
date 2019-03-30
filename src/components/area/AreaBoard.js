047c35691e12970c21d3ba266ddb46ef5310ae54
Sultan Kadyrkesh <sultankadyr@outlook.com>
2019-03-31 00:17:31 +0600

    AreaBoard с реальными данными

--- src/components/area/AreaBoard.js
+++ src/components/area/AreaBoard.js
@@ -5,6 +5,10 @@ import { Grid, Typography, Link} from '@material-ui/core';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import YandexShare from '../layout/YandexShare';
 
 const styles = theme => ({
     icon: {
@@ -27,99 +31,75 @@ const styles = theme => ({
 class AreaBoard extends React.Component {
     render() {
 
-        const { classes,theme } = this.props;
-
+        const { classes,theme, area, areaTypes, sportTypes } = this.props;
+        const areaType = area.id && (areaTypes && areaTypes.find(item => item.id == area.type.id));
+        const sports = area.id && (sportTypes && sportTypes.filter(item => area.sportTypes.find(i => i.id == item.id) != undefined));
+        console.log('areaType', areaTypes);
+        console.log('sports', sports);
         return <React.Fragment>
             <div className={classNames(classes.layout, classes.cardGrid)}>
-            <Grid container>
-                <Grid item xs={12} md={6}>
-                <h2 style={{ fontWeight: '400' }}>Ледовая площадка ОК "Лужники"</h2>
-                <h5 style={{ fontWeight: '400' }}>Входит в состав "Лужники"</h5>
-                </Grid>
-                <Grid item xs={12} md={6}>
+            <Grid container justify='space-between'>
+                <Grid item xs={12} sm={6}>
+                <h2 style={{ fontWeight: '400' }}>{area.id && area.name}</h2>
+                <h3><YandexShare /></h3>
                 </Grid>
             </Grid>
             <Carousel showArrows={true} autoPlay={true} showThumbs={false} dynamicHeight={false} useKeyboardArrows={true} swipeable={true} emulateTouch={true} showStatus={false}>
-          <div >
-            <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
-            <p className="legend" style={{backgroundColor:'#0000'}}>Legend 1</p>
-          </div>
-          <div>
-          <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
-            <p className="legend" style={{backgroundColor:'#0000'}}>Legend 2</p>
-          </div>
-          <div>
-          <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
-            <p className="legend" style={{backgroundColor:'#0000'}}>Legend 3</p>
-          </div>
-          <div>
-          <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
-            <p className="legend" style={{backgroundColor:'#0000'}}>Legend 4</p>
-          </div>
-          <div>
-          <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
-            <p className="legend" style={{backgroundColor:'#0000'}}>Legend 5</p>
-          </div>
-          <div>
-          <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
-            <p className="legend" style={{backgroundColor:'#0000'}}>Legend 6</p>
-          </div>
+            {
+                area.id && area.images.map(item => {
+                    return <div key={item.filename}>
+                        <img src={item.url} style={{height:'300px',objectFit:'cover'}} />
+                    </div>
+                })
+            }
         </Carousel>
             <br/>
             <Grid container spacing={24}>
                 <Grid item xs={12} md={6} style={{paddingLeft:'30px'}}>
-                    <Typography component="p"><i class="fas fa-location-arrow"></i> Москва, ул.Пушкина 12/1</Typography>
-                    <Typography component="p"><i class="fas fa-subway"></i> Темирязевская</Typography>
-                    <Typography component="p"><i class="fas fa-subway"></i> Петровско-Разумовская</Typography>
-                </Grid>
-                <Grid item xs={12} md={6} style={{paddingRight:'30px'}}>
-                    <Typography component="p" style={{textAlign:'right'}}><i class="fas fa-phone"></i> +7 (903) 567-89-78,+(903)-567-89-79</Typography>
-                    <Typography component="p" style={{textAlign:'right'}}><i class="fas fa-desktop"></i> <Link>www.luzhniki.ru</Link></Typography>
+                    <Typography component="p"><i className="fas fa-location-arrow"></i> {area.id && area.location.address}</Typography>                    
                 </Grid>
                 <Grid item xs={12}>
                     <blockquote>
                         <Typography component="p" align="justify">
-                        Малая спортивная арена Олимпийского комплекса «Лужники» (ранее Центрального стадиона им. В. И. Ленина) — крытый стадион вместимостью 8700 мест, расположенный в Москве. Арена была построена в 1956 году в СССР. Она принимала волейбольные соревнования на летних Олимпийских играх 1980 года[1]. На ней также проходили соревнования в рамках летней Универсиады 1973 года, Игр доброй воли 1986 года, Спартакиад народов СССР и других турниров.
+                        {area.id && area.description}
                         </Typography>
                     </blockquote>
                 </Grid>
                 <Grid item xs={12} md={3}>
                 <Typography component="h4"><b>Режим работы</b></Typography><br/>
-                <Typography component="p" ><b> пн-сб</b> 06:30-23:30</Typography>
-                <Typography component="p" ><b> вс</b> выходной</Typography>  
-                <Typography component="p" ><b> перерыв</b> 12:00-14:00</Typography>
+                <Typography component="p" >{area.id && area.worktime}</Typography>
                 </Grid>
                 <Grid item xs={12} md={3}>
                 <Typography component="h4"><b>Характеристики</b></Typography><br/>
-                <Typography component="p" ><b> Тип</b> Крытая</Typography>  
+                <Typography component="p" ><b> Тип площадки:</b> {areaType && areaType.name}</Typography>  
                 </Grid>
                 <Grid item xs={12} md={3}>
                 <Typography component="h4"><b>Стоимость</b></Typography><br/>
-                <Typography component="p" > от 3000 тг/час</Typography>
+                <Typography component="p" > {area.id && area.price}</Typography>
                 
                 </Grid>
                 <Grid item xs={12} md={3}>
                 <Typography component="h4"><b>Контакты</b></Typography><br/>
-                <Typography component="p" > Иванов Иван Иванович</Typography>
+                <Typography component="p" > {area.id && area.contacts}</Typography>
                 
                 </Grid>
                 <Grid item xs={2} style={{textAlign:'center',letterSpacing:'40px'}}>
-                <i class="fas fa-lightbulb" style={{fontSize:'28px',color:'lightgreen'}}></i>
+                <i className="fas fa-lightbulb" style={{fontSize:'28px',color:'lightgreen'}}></i>
                 </Grid>
                 <Grid item xs={2} style={{textAlign:'center',letterSpacing:'40px'}}>
-                <i class="fas fa-wifi" style={{fontSize:'28px',color:'lightgreen'}}></i>
+                <i className="fas fa-wifi" style={{fontSize:'28px',color:'lightgreen'}}></i>
                 </Grid>
                 <Grid item xs={2} style={{textAlign:'center',letterSpacing:'40px'}}>
-                <i class="fas fa-coffee" style={{fontSize:'28px',color:'lightgreen'}}></i>
+                <i className="fas fa-coffee" style={{fontSize:'28px',color:'lightgreen'}}></i>
                 </Grid>
                 <Grid item xs={2} style={{textAlign:'center',letterSpacing:'40px'}}>
-                <i class="fas fa-car" style={{fontSize:'28px',color:'lightgreen'}}></i>
+                <i className="fas fa-car" style={{fontSize:'28px',color:'lightgreen'}}></i>
                 </Grid>
                 <Grid item xs={2} style={{textAlign:'center',letterSpacing:'40px'}}>
-                <i class="fas fa-medkit" style={{fontSize:'28px',color:'lightgreen'}}></i>
+                <i className="fas fa-medkit" style={{fontSize:'28px',color:'lightgreen'}}></i>
                 </Grid>
                 <Grid item xs={2} style={{textAlign:'center',letterSpacing:'40px'}}>
-                <i class="fas fa-shower" style={{fontSize:'28px',color:'lightgreen'}}></i>
+                <i className="fas fa-shower" style={{fontSize:'28px',color:'lightgreen'}}></i>
                 </Grid>
                 
             </Grid>
@@ -133,4 +113,19 @@ AreaBoard.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(AreaBoard)
+const mapStateToProps = (state) => {
+    console.log(state);
+    return {
+        area: state.area.selectArea,
+        sportTypes: state.firestore.ordered.categorySports,
+        areaTypes: state.firestore.ordered.sporgroundstype
+    }
+}
+export default compose(
+    withStyles(styles),
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'categorySports' },
+        { collection: 'sportgroundstype'}
+    ])
+)(AreaBoard)
