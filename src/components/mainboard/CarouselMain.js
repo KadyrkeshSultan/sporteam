71d63b08a3750d7e6fbcd13a2b28bf79c2686db2
Sultan Kadyrkesh <sultankadyr@outlook.com>
2019-04-02 19:21:51 +0600

    Новости из Firebase

--- src/components/mainboard/CarouselMain.js
+++ src/components/mainboard/CarouselMain.js
@@ -1,44 +1,68 @@
 import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import { firestoreConnect } from 'react-redux-firebase';
 
-const styles ={
-    backgroundColor: '#0000',
-    fontSize: 'larger'
-}
-export default class CarouselMain extends Component {
+const styles = theme =>({
+    titleNews:{
+        backgroundColor: '#0000',
+        fontSize: 'larger',
+    },
+    [theme.breakpoints.down('sm')]: {
+        imageNews: {
+            height: '300px',
+            objectFit: 'cover'
+        }
+    },
+    [theme.breakpoints.up('md')]: {
+        imageNews: {
+            height: '400px',
+            objectFit: 'cover'
+        }
+    },
+})
+class CarouselMain extends Component {
   render() {
+      const {news, classes} = this.props;
     return (
 
       <div ><br />
         <h2 style={{ fontWeight: '400' }}>ПОСЛЕДНИЕ НОВОСТИ</h2>
         <Carousel showArrows={true} autoPlay={true} showThumbs={false} dynamicHeight={false} useKeyboardArrows={true} swipeable={true} emulateTouch={true} showStatus={false} infiniteLoop>
-          <div >
-            <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
-            <p className="legend" style={styles}>Заголовок новости 1</p>
-          </div>
-          <div>
-          <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
-            <p className="legend" style={styles}>Заголовок новости 2</p>
-          </div>
-          <div>
-          <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
-            <p className="legend" style={styles}>Заголовок новости 3</p>
-          </div>
-          <div>
-          <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
-            <p className="legend" style={styles}>Заголовок новости 4</p>
-          </div>
-          <div>
-          <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
-            <p className="legend" style={styles}>Заголовок новости 5</p>
-          </div>
-          <div>
-          <img src="https://itd2.mycdn.me/image?id=850604732307&t=20&plc=WEB&tkn=*wBRLLaXtkm34eqMg1mSbUIPEvO0" style={{height:'300px',objectFit:'cover'}}/>
-            <p className="legend" style={styles}>Заголовок новости 6</p>
-          </div>
+        {
+            news && news.map(item =>{
+                return <div key={item.id}>
+                    <img src={item.images[0]} className={classes.imageNews} />
+                    <p className='legend' style={{backgroundColor: '#0000', fontSize: 'larger'}}>{item.title}</p>
+                </div>
+            })
+        }
         </Carousel>
       </div >
     );
   }
 }
+
+const mapStateToProps = (state) => {
+    console.log("main", state);
+    return {
+        news: state.firestore.ordered.news,
+    }
+}
+
+export default compose(
+    withStyles(styles),
+    connect(mapStateToProps),
+    firestoreConnect([
+        {
+            collection: 'news',
+            orderBy: [
+                ['datepublish', 'desc']
+            ],
+            limit: 5
+        },
+    ])
+)(CarouselMain);
