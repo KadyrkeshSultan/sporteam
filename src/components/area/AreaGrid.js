52ddd3df687303b6c0ed7b462296de610afa00fa
interglare <interglare@gmail.com>
2019-04-16 22:09:13 +0600

    Добавил кнопу по переходу ко всем контентам

--- src/components/area/AreaGrid.js
+++ src/components/area/AreaGrid.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import AreaCard from './AreaCard';
 import { Grid } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+    button: {
+      margin: theme.spacing.unit,
+    },
+    input: {
+      display: 'none',
+    },
+  });
 
 class AreaGrid extends React.Component {
 
     render() {
         const { areas } = this.props;
         return <React.Fragment>
-            <h2 style={{ fontWeight: '400' }}>ПОПУЛЯРНЫЕ ПЛОЩАДКИ</h2>
-
             <Grid container spacing={24}>
+            <Grid item xs={12} md={6}>
+                    <h2 style={{ fontWeight: '300',marginBlockEnd: 0 }}>ПОПУЛЯРНЫЕ ПЛОЩАДКИ</h2>
+                </Grid>
+                <Grid item xs={12} md={6} style={{textAlign:'right'}}>
+                    <Button  href='areas' color="primary" style={{
+                        marginBlockStart: '1.3em',
+                        textDecoration: 'none',
+                        color:'black'
+                        
+                    }}>Все площадки &nbsp;<i class="fas fa-arrow-right" style={{fontSize:'inherit'}}> </i></Button >
+                </Grid>
                 {
                     areas && areas.map((item) => {
                         return <Grid key={item.id} item xs={12} sm={6} md={6} lg={3}>
@@ -21,4 +41,4 @@ class AreaGrid extends React.Component {
         </React.Fragment>
     }
 }
-export default AreaGrid;
+export default withStyles(styles)(AreaGrid);
--- src/components/event/EventsGrid.js
+++ src/components/event/EventsGrid.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import EventCard from './EventCard';
 import { Grid } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+    button: {
+      margin: theme.spacing.unit,
+    },
+    input: {
+      display: 'none',
+    },
+  });
 
 class EventsGrid extends React.Component {
     render() {
         const { events } = this.props;
 
         return <React.Fragment>
-            <h2 style={{ fontWeight: '400' }}>БЛИЖАЙШИЕ МЕРОПРИЯТИЯ</h2>
-
             <Grid container spacing={24}>
+                <Grid item xs={12} md={6}>
+                    <h2 style={{ fontWeight: '300',marginBlockEnd: 0 }}>БЛИЖАЙШИЕ МЕРОПРИЯТИЯ</h2>
+                </Grid>
+                <Grid item xs={12} md={6} style={{textAlign:'right'}}>
+                    <Button  href='events' color="primary" style={{
+                        marginBlockStart: '1.3em',
+                        textDecoration: 'none',
+                        color:'black'
+                        
+                    }}>Все мероприятия &nbsp;<i class="fas fa-arrow-right" style={{fontSize:'inherit'}}> </i></Button >
+                </Grid>
                 {
                     events && events.map((item) => {
                         return <Grid key={item.id} item xs={12} sm={6} md={6} lg={3}>
@@ -23,4 +43,4 @@ class EventsGrid extends React.Component {
         </React.Fragment>
     }
 }
-export default EventsGrid;
+export default withStyles(styles)(EventsGrid);
