db783ba5e890a00e59beb04cf5520b910c460d2b
Sultan Kadyrkesh <sultankadyr@outlook.com>
2019-05-15 12:02:32 +0600

    Удалил лишние компоненты

--- src/components/layout/ButtonAppBar.js
+++ src/components/layout/ButtonAppBar.js
@@ -5,16 +5,11 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-import Button from '@material-ui/core/Button';
-import SignedInLinks from './SignedInLinks';
-import SignedOutLinks from './SignedOutLinks';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
-import { Link } from '@material-ui/core';
 import {signOut} from '../../store/actions/authActions'
 
 const styles = {
@@ -54,7 +49,7 @@ class ButtonAppBar extends React.Component {
     }
 
     render() {
-        const { classes, auth, signOut } = this.props;
+        const { classes, auth } = this.props;
         const { anchorEl } = this.state;
         const open = Boolean(anchorEl);
         return (
